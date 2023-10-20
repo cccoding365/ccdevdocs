78ed57b801f39620e008e7a1ed4ea1039a25ded7
DoubledConG <992196871@qq.com>
2023-10-20 22:48:15 +0800

    特性: 新增Project页面

--- docs/.vitepress/nav.config.js
+++ docs/.vitepress/nav.config.js
@@ -4,4 +4,5 @@ export default [
 	{ text: "Library", link: "/library/" },
 	{ text: "Website", link: "/website/" },
 	{ text: "Reading", link: "/reading/" },
+	{ text: "Project", link: "/project/" },
 ];
