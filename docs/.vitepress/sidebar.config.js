dbd5197d9054f945791383aac80e1b4ccdb228a0
DoubledConG <992196871@qq.com>
2023-10-20 22:41:34 +0800

    文档: 修改文档内容

--- docs/.vitepress/sidebar.config.js
+++ docs/.vitepress/sidebar.config.js
@@ -1,81 +1,112 @@
 export default {
 	"/frontend/": [
 		{
-			text: "Front end",
+			text: "Internet",
+			link: "/frontend/Internet/",
+		},
+		{
+			text: "HTML",
+			link: "/frontend/HTML/",
+		},
+		{
+			text: "CSS",
+			link: "/frontend/CSS/",
 			items: [
 				{
-					text: "Internet",
-					link: "/frontend/Internet/",
+					text: "Flex Layout Guide",
+					link: "/frontend/CSS/flex-layout-guide.md"
 				},
 				{
-					text: "HTML",
-					link: "/frontend/HTML/",
-				},
-				{
-					text: "CSS",
-					link: "/frontend/CSS/",
-				},
-				{
-					text: "JavaScript",
-					link: "/frontend/JavaScript/",
-				},
-				{
-					text: "Version Control System",
-					link: "/frontend/VersionControlSystem/",
-					items: [
-						{
-							text: "Git",
-							link: "/frontend/VersionControlSystem/Git/",
-							items: [
-								{
-									text: "git-cheatsheet",
-									link: "/frontend/VersionControlSystem/Git/git-cheatsheet.md"
-								}
-							]
-						}
-					]
-				},
-				{
-					text: "Package Managers",
-					link: "/frontend/PackageManagers/",
-				},
-				{
-					text: "Vue",
-					link: "/frontend/Vue/",
-				},
-				{
-					text: "React",
-					link: "/frontend/React/",
-				},
-				{
-					text: "Markdown",
-					link: "/frontend/Markdown/",
-				},
-				{
-					text: "Build Tools",
-					link: "/frontend/BuildTools/",
-				},
-				{
-					text: "TypeScript",
-					link: "/frontend/TypeScript/",
-				},
-				{
-					text: "Server Side Rendering",
-					link: "/frontend/ServerSideRendering/",
-				},
-				{
-					text: "Desktop Applications",
-					link: "/frontend/DesktopApplications/",
-				},
-				{
-					text: "Mobile Applications",
-					link: "/frontend/MobileApplications/",
-				},
-				{
-					text: "Node.js",
-					link: "/frontend/Nodejs/",
-				},
-			],
+					text: "Library",
+					link: "/frontend/CSS/library.md"
+				}
+			]
 		},
+		{
+			text: "JavaScript",
+			link: "/frontend/JavaScript/",
+			items: [
+				{
+					text: "ES6+",
+					link: "/frontend/JavaScript/ES6/"
+				},
+				{
+					text: "Fetch API",
+					link: "/frontend/JavaScript/javascript-fetch-api.md"
+				}
+			]
+		},
+		{
+			text: "Version Control System",
+			link: "/frontend/VersionControlSystem/",
+			items: [
+				{
+					text: "Git",
+					link: "/frontend/VersionControlSystem/Git/"
+				},
+				{
+					text: "Git Cheatsheet",
+					link: "/frontend/VersionControlSystem/Git/git-cheatsheet.md"
+				}
+			]
+		},
+		{
+			text: "Package Managers",
+			link: "/frontend/PackageManagers/",
+			items: [
+				{
+					text: "package.json",
+					link: "/frontend/PackageManagers/package-json.md"
+				}
+			]
+		},
+		{
+			text: "Vue",
+			link: "/frontend/Vue/",
+		},
+		{
+			text: "React",
+			link: "/frontend/React/",
+		},
+		{
+			text: "Markdown",
+			link: "/frontend/Markdown/",
+			item: [
+				{
+					text: "Markdown Cheatsheet",
+					link: "/frontend/Markdown/markdown-cheat-sheet.md"
+				}
+			]
+		},
+		{
+			text: "Build Tools",
+			link: "/frontend/BuildTools/",
+		},
+		{
+			text: "TypeScript",
+			link: "/frontend/TypeScript/",
+		},
+		{
+			text: "Server Side Rendering",
+			link: "/frontend/ServerSideRendering/",
+		},
+		{
+			text: "Desktop Applications",
+			link: "/frontend/DesktopApplications/",
+		},
+		{
+			text: "Mobile Applications",
+			link: "/frontend/MobileApplications/",
+			items: [
+				{
+					text: "uni app",
+					link: "/docs/frontend/MobileApplications/uni-app.md"
+				}
+			]
+		},
+		{
+			text: "Node.js",
+			link: "/frontend/Nodejs/",
+		}
 	]
 };
